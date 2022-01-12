d2920616ae08e2f7aa0596a1c98d305a7d409f70
Kristen <kriscadacio@gmail.com>
2022-01-12 16:59:28 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+let finalCard;
+
+function writeCards(arr,event) {
+
+    let message = [];
+
+    for (let i = 0; i < arr.length;i++) {
+        message.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+    }
+
+return message;
+
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+
+function countDown(num) {
+    let i = num;
+    while (i >= 0) {
+        console.log (i);
+        i--;
+    }
+    return countDown
+}
